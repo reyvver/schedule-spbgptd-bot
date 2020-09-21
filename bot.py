cfea24333a55328de7d577362dcefd3cf94bc0ae
Tatiana Dragunova <49490142+reyvver@users.noreply.github.com>
2020-09-21 20:42:30 +0300

    new code

--- bot.py
+++ bot.py
@@ -1,22 +1,94 @@
 import telebot
+from telebot import types
 import config
+import schedule_manager
 
 bot = telebot.TeleBot(config.TELEGRAM_TOKEN)
+timetable = schedule_manager
 
 
 ############################################
 
+
 # handler for command /start
 @bot.message_handler(commands=['start'])
 def on_start(message):
-    bot.reply_to(message, "hi")
+    get_data()
+    bot.send_message(
+        message.chat.id,
+        "Начнем работу, оки?",
+        reply_markup=main_keyboard()
+    )
+
+
+# Show keyboard
+def main_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+
+    button_1 = types.KeyboardButton('Сегодня')
+    button_2 = types.KeyboardButton('Завтра')
+    button_3 = types.KeyboardButton('Эта неделя')
+    button_4 = types.KeyboardButton('Следующая')
+
+    keyboard.add(button_1, button_2, button_3, button_4)
+    return keyboard
+
+
+# Handler for keyboard buttons
+@bot.message_handler(content_types=['text'])
+def on_text(message):
+    if message.text.lower() == "сегодня":
+        on_today(message)
+    if message.text.lower() == "завтра":
+        on_tomorrow(message)
+    if message.text.lower() == "эта неделя":
+        on_current(message)
+    if message.text.lower() == "следующая":
+        on_next(message)
+
+
+# Getting started with sheet's data
+def get_data():
+    timetable.on_start_schedule()
+
+
+def on_today(message):
+    bot.reply_to(message, timetable.print_day_schedule("сегодня"), parse_mode="Markdown")
+
+
+# Handler for button 'current week'
+def on_current(message):
+    bot.send_message(message.chat.id, timetable.print_week_schedule("текущая"), parse_mode="Markdown")
+
+
+# Handler for button 'today'
+def on_next(message):
+    bot.send_message(message.chat.id, timetable.print_week_schedule("следующая"), parse_mode="Markdown")
+
+
+# Handler for button 'tomorrow'
+def on_tomorrow(message):
+    bot.reply_to(message, timetable.print_day_schedule("завтра"), parse_mode="Markdown")
+
+
+# # Handler for button 'today'
+# def on_today():
+#     timetable.print_day_schedule("сегодня")
+#
+#
+# # Handler for button 'tomorrow'
+# def on_tomorrow():
+#     timetable.print_day_schedule("завтра")
+#
+#
+# # Handler for button 'current week'
+# def on_current():
+#     timetable.load_data_from_sheet()
+#
+#
+# # Handler for button 'today'
+# def on_next():
+#     timetable.load_data_from_sheet()
 
 
 bot.polling(none_stop=True)
-
-
-# @bot.message_handler(content_types=['text'])
-# def on_text(message):
-#     if message == "Сегодня":
-#         day_of_week = datetime.today().weekday()
-#         bot.reply_to(message, "todos " + str(day_of_week))
--- schedule_manager.py
+++ schedule_manager.py
@@ -1,5 +1,6 @@
 import gspread
 import config
+import datetime
 from schedule_models import *
 
 gc = gspread.service_account()
@@ -8,8 +9,11 @@ current_worksheet = table.sheet1
 
 values: List[List] = []
 days_of_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
-current_time: str
+emoticons_books = ["\U0001F4D5", "\U0001F4D7", "\U0001F4D8", "\U0001F4D9", "\U0001F4D2", "\U0001F4D4"]
+# emoticons_books = [":closed_book:", ":green_book:", ":blue_book:", ":orange_book:", ":ledger:", "U+1F4D4"]
+current_time: str = ""
 schedule: WeekSchedule
+week_type: bool
 
 
 ######################################################
@@ -20,14 +24,30 @@ def load_data_from_sheet():
     values = current_worksheet.get_all_values()
 
 
+# Четная или нечетная неделя
+# true - четная
+# false - нечетная
+def type_of_current_week():
+    global week_type
+    number = datetime.datetime.today().isocalendar()[1]
+    if number % 2 == 0:
+        print("нечетная")
+        week_type = False
+    else:
+        print("четная")
+        week_type = True
+
+
+
 # Создать запись
 def initialize_item(current_row: List):
     global current_time
 
-    if current_time == "":
+    if current_row[1] != "":
         current_time = current_row[1]
 
     row_time = current_time
+
     if current_row[2] != "":
         row_time = current_row[2]
 
@@ -50,15 +70,22 @@ def get_items_couple(numerator_row: List, denominator_row: List):
 
 # Получение расписания на день. У каждого дня 12 строчек
 def initialize_day_schedule(first_row_of_day: int):
+    global values
     global current_time
     day_schedule: List[ClassItemCouple] = []
     counter = first_row_of_day
-    while counter < 13:
-        current_time = ""
-        numerator = values[counter]
-        denominator = values[counter + 1]
-        day_schedule.append(get_items_couple(numerator, denominator))
+    end = first_row_of_day + 11
+
+    while counter < end:
+        if (counter < len(values)) & (counter + 1 < len(values)):
+            # print(str(counter) + "   " + str(counter + 1))
+            current_time = ""
+            numerator = values[counter]
+            denominator = values[counter + 1]
+
+            day_schedule.append(get_items_couple(numerator, denominator))
         counter = counter + 2
+
     return DaySchedule(
         class_couples=day_schedule
     )
@@ -66,110 +93,92 @@ def initialize_day_schedule(first_row_of_day: int):
 
 # Получение расписания недели
 def initialize_week_schedule():
+    global schedule
     row_number: int = 1
     days_schedule: List[DaySchedule] = []
+
     for i in range(6):
         days_schedule.append(initialize_day_schedule(row_number))
         row_number = row_number + 12
-    return WeekSchedule(
-        days_schedule=days_schedule
-    )
+
+    schedule = WeekSchedule(days_schedule=days_schedule)
+    # return WeekSchedule(
+    #     days_schedule=days_schedule
+    # )
 
 
-def print_selected_item(item: ClassItem):
-    print(
-        item.time_range + "  " + item.group_index + "  " + item.class_name + "  " + item.class_type + "  " + item.location + "   " + item.teacher)
+# Возвращает строку со полями дня
+def get_day(item: ClassItem):
+    result: str = ""
+    if item.class_name != "":
+        # result = item.time_range + " " + item.class_name + " (" + item.class_type + ") \n" + item.location + \
+        #          "\nПреподаватель:" + item.teacher + " \n\n"
+        result = "*• " +item.time_range + "* " + item.class_name + " (" + item.class_type + ") \n" + item.location + \
+                 "\nПреподаватель:" + item.teacher + " \n\n"
+    return result
 
 
+# Печать расписания на выбранный день (включает проверку на четность / нечетность
 def print_selected_day(number: int):
+    global week_type
     current_schedule: DaySchedule = schedule.days_schedule[number]
-    print("Выбранный день  " + days_of_week[number])
+    result: str = ""
 
     for i in range(len(current_schedule.class_couples)):
         current_couple: ClassItemCouple = current_schedule.class_couples[i]
-        print_selected_item(current_couple.numerator)
-        print_selected_item(current_couple.denominator)
+        day: str = ""
+        if week_type:
+            day = get_day(current_couple.denominator)
+        else:
+            day = get_day(current_couple.numerator)
+        result = result + day
+    return result
 
 
-load_data_from_sheet()
-schedule = initialize_week_schedule()
-print_selected_day(0)
-# global current_day_of_week
-# current_day_of_week = day_of_week[i]
+# Печать расписания на сегодня / завтра
+def print_day_schedule(type_of_day: str):
+    global week_type
+    current_type = week_type
+    current_week_number = datetime.datetime.today().isocalendar()[1]
+    day: datetime.date = datetime.datetime.today()
+
+    if type_of_day == "завтра":
+        day = day + datetime.timedelta(days=1)
+        number = day.isocalendar()[1]
+        if number != current_week_number:
+            week_type = not week_type
+
+    day_of_week: int = day.weekday()
+
+    if day_of_week != 6:
+        result = print_selected_day(day_of_week)
+        if week_type != current_type:
+            week_type = current_type
+        return result
+    else:
+        return "уухадноу"
 
 
-# load_data_from_sheet()
-# for i in range(12):
-#     print(i)
+def print_week_schedule(type_of_week: str):
+    global week_type
+    current_type = week_type
+    result: str = ""
 
-# for i in range(len(values[2])):
-#     print(i)
-#     print(values[2][i])
-#
-# week_schedule: WeekSchedule
-#
-#
-# # Забрать расписание из облака
-# def update_schedule_from_cloud():
-#     global week_schedule
-#     week_schedule = load_week_schedule()
-#
-#
-# # Возвращает ClassItem
-# def load_class_item(cell_number: int):
-#     class_data = table.sheet1.range('B{0}:H{0}'.format(cell_number))
-#
-#     class_item = ClassItem(time_range=class_data[0].value,
-#                            group_index=class_data[2].value,
-#                            class_name=class_data[3].value,
-#                            class_type=class_data[4].value,
-#                            location=class_data[5].value,
-#                            teacher=class_data[6].value)
-#
-#     extra_time_range = class_data[1].value
-#     if extra_time_range != "":
-#         class_item.time_range = extra_time_range
-#
-#     if class_item.time_range == "":
-#         time_index = cell_number - 1
-#         class_item.time_range = table.sheet1.get("B" + str(time_index)).first()
-#
-#     return class_item
-#
-#
-# # Возвращает пару для четной и нечетной недели
-# def load_class_couple(cell_number: int):
-#     even = load_class_item(cell_number)
-#     odd = load_class_item(cell_number + 1)
-#
-#     return ClassItemCouple(even, odd)
-#
-#
-# # Возвращает расписание на день
-# def load_day_schedule(day_number: int):
-#     day_schedule: DaySchedule = DaySchedule()
-#
-#     i = 2 + ((day_number - 1) * 12)
-#     print("i: " + str(i))
-#     target = i + 12
-#     print("target: " + str(target))
-#
-#     while i < target:
-#         day_schedule.class_couples.append(load_class_couple(i))
-#         i += 2
-#
-#     return day_schedule
-#
-#
-# #  Возвращает расписание на неделю
-# def load_week_schedule():
-#     return WeekSchedule(
-#         monday=load_day_schedule(1),
-#         tuesday=load_day_schedule(2),
-#         wednesday=load_day_schedule(3),
-#         thursday=load_day_schedule(4),
-#         friday=load_day_schedule(5),
-#         saturday=load_day_schedule(6)
-#     )
+    if type_of_week == "следующая":
+        week_type = not week_type
 
-#####################################################
+    for i in range(6):
+        result = result + emoticons_books[i] + " *" + days_of_week[i] + "* \n" + print_selected_day(i) + "\n\n"
+
+    week_type = current_type
+    return result
+
+
+# Начало работы с данными
+def on_start_schedule():
+    load_data_from_sheet()
+    initialize_week_schedule()
+    type_of_current_week()
+
+# on_start_schedule()
+# print(print_day_schedule("сегодня"))
