ee9adb29d5c3978456ef771b15fd10f772cd953a
Tatiana Dragunova <49490142+reyvver@users.noreply.github.com>
2021-04-07 00:14:04 +0300

    pre-final changes
    + edited short and full view
    + fixed statistics

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ TELEGRAM_TOKEN: str = '1391745018:AAG4cVjkrF7LAPUQst-lslEZd5Fz9r0aDuM'
 gc = gspread.service_account()
 
 SHEET_KEY = '1XPcQ6WMOQpA5M8gnAd58HuhfuJONkXOOAJPDEMWf_8c'
+
+group_list = ["1-ТИД-3", "1-ГДА-10", "1-АДА-9"]
