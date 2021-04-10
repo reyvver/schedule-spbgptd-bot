6c6182c43fc525712b83b9fd054e84b9ebc3acef
Tatiana Dragunova <49490142+reyvver@users.noreply.github.com>
2021-04-10 18:50:28 +0300

    Clear inf

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import gspread
 
-# основной бот:
-# TELEGRAM_TOKEN: str = '1333510013:AAFEgog3vcn_QtQWDITyrOyqlZXH-I23l68'
-# gc = gspread.service_account(filename='service_account.json')
-
 # тестовый бот:
 TELEGRAM_TOKEN: str = '1391745018:AAG4cVjkrF7LAPUQst-lslEZd5Fz9r0aDuM'
 gc = gspread.service_account()
